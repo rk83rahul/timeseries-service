40dd7cf838426f4576b86907d0d77e7ea3c100ca
Christer Gjerstad <christer.gjerstad@yara.com>
2021-03-03 08:38:24 +0100

    Update GreetingResource.java

--- src/main/java/com/yara/GreetingResource.java
+++ src/main/java/com/yara/GreetingResource.java
@@ -1,6 +1,7 @@
 package com.yara;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,6 +13,12 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello Time Series Data";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "Hello git format-patch";
     }
 
     
